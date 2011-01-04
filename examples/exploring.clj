acf1401a51aa8fb4de0fa9bbd7b0e9ab5fe3e032
Manuel Joseph <jmpak80@gmail.com>
2011-01-04 13:51:48 +0530

    progress

--- examples/exploring.clj
+++ examples/exploring.clj
@@ -1,3 +1,27 @@
 (ns examples.exploring
     (:use clojure.contrib.str-utils)
     (:import (java.io File)))
+    
+(defn is-small? [number]
+    (if (< number 1000) "yes" "no"))
+    
+(defn is-small? [number]
+    (if (< number 1000)
+        (do (println "hello" number) "yes")
+        "no"))
+    
+(defn countdown [result x]
+    (if (zero? x)
+        result
+        (recur (conj result x) (dec x))))
+    
+(defn indexed [coll] (map vector (iterate inc 0) coll))
+
+(defn index-filter [pred coll]
+    (when pred
+        (for [[idx elt] (indexed coll) :when (pred elt)] idx)))
+        
+(defn index-of-any [pred coll]
+    (first (index-filter pred coll)))        
+
+    
